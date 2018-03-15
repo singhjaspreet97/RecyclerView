be03391b36ceec77887051cd448952948b7dd7b9
singhjaspreet97 <jassi80singh80@gmail.com>
2018-03-16 02:57:25 +0530

    RecyclerView

---
+++ app/src/main/java/com/example/jaspreet/recyclerviewproject/ListItem.java
@@ -0,0 +1,24 @@
+package com.example.jaspreet.recyclerviewproject;
+
+/**
+ * Created by Jaspreet on 29-01-2018.
+ */
+
+public class ListItem {
+
+    private String head;
+    private String desc;
+
+    public ListItem(String head, String desc) {
+        this.head = head;
+        this.desc = desc;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
---
+++ app/src/main/java/com/example/jaspreet/recyclerviewproject/MyAdapter.java
@@ -0,0 +1,64 @@
+package com.example.jaspreet.recyclerviewproject;
+
+import android.app.LauncherActivity;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Jaspreet on 29-01-2018.
+ */
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+
+    private List<ListItem> listItems;
+    private Context context;
+
+    public MyAdapter(List<ListItem> listItems, Context context) {
+        this.listItems = listItems;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+      View v = LayoutInflater.from(parent.getContext())
+              .inflate(R.layout.list_item, parent, false);
+                      return new  ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+            ListItem listItem =  listItems.get(position);
+
+            holder.textViewHead.setText(listItem.getHead());
+            holder.textViewDesc.setText(listItem.getDesc());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView textViewHead;
+        public TextView textViewDesc;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewHead = (TextView) textViewHead.findViewById(R.id.textViewHead);
+            textViewDesc = (TextView) textViewDesc.findViewById(R.id.textViewDesc);
+        }
+    }
+
+
+}
+
+
